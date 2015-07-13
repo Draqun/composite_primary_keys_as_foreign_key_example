bc6a1906fcdff957379c063accb4f1edb3b1d1b0
Damian Giebas <dig271090@gmail.com>
2015-07-13 14:57:45 +0200

    Remove binding.pry

--- spec/models/child.rb
+++ spec/models/child.rb
@@ -4,7 +4,6 @@ RSpec.describe ::Child, type: :model do
   before(:all) do
     @grand = FactoryGirl.create(:grand)
     @parent = FactoryGirl.create(:parent, grand: @grand)
-    binding.pry
     @child = FactoryGirl.create(:child, parent_id: @parent.id[0][1], grand_id: @grand.id)
   end
 
